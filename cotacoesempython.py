af73b2b5de9d66dc0dd59c44159511b8f0d54b45
JAILSON COSTA GARRIDO <jailsongarrido23@gmail.com>
2024-05-14 20:30:11 -0300

    Delete cotacoesempython.py

--- cotacoesempython.py
+++
@@ -1,7 +0,0 @@
-import pandas
-from pandas_datareader import data as pdr
-import yfinance as yfin
-yfin.pdr_override()
-
-df = pdr.get_data_yahoo("TSLA", start="2020-01-01", end="2021-01-01")
-print(df)
